db82e1de1e1ea8d91573f75c623bb0daa3ac436c
dbetebenner <dbetebenner@gmail.com>
2015-10-22 14:30:56 -0400

    Final push for version 1.0

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <- 
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('RLIdata ',paste(paste(unlist(strsplit(as.character(packageVersion("RLIdata")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),'  For help type: help("RLIdata")')
+		packageStartupMessage('RLImatrices ',paste(paste(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),'  For help type: help("RLImatrices")')
 	}
 }
