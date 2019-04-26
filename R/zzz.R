cc332badb112e5e72d2d7d46092c845f8aa0024a
Damian Betebenner <dbetebenner@nciea.org>
2019-04-26 09:05:05 -0400

    Adding in READING_UNIFIED matrices to package and bumping version number to 10.0-0.0

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (7-17-2017). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
+		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (4-26-2019). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
 	}
 }
