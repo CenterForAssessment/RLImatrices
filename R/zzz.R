60ba2a754260d14fb276ea09cd93bdd386176130
Damian Betebenner <dbetebenner@nciea.org>
2019-05-23 16:47:20 -0400

    Adding in new matrices for STAR and RASCH using data up to 2017_2018.3

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (4-26-2019). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
+		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (5-23-2019). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
 	}
 }
