ed434a33619d618c6fc802c83bf07ed39afc7568
Damian Betebenner <dbetebenner@nciea.org>
2020-05-27 23:07:02 -0400

    Updating matrices removing two low N matrices from SPANISH matrices

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (5-3-2020). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
+		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (5-28-2020). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
 	}
 }
