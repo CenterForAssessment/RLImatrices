8b28a205c4abe4a45a5bce73c753a60b9fdf9dc4
Damian Betebenner <dbetebenner@nciea.org>
2023-03-20 05:23:32 -0400

    Fixing up RLI_UK matrices

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (2-20-2023). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
+		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (3-20-2023). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
 	}
 }
