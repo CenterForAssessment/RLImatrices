9e0f57ad4812a5a2d18924d944825b13567f44dc
Damian Betebenner <dbetebenner@nciea.org>
2025-03-17 12:21:22 -0400

    Update and fixing of previous commit

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (2-25-2025). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
+		packageStartupMessage('RLImatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("RLImatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (3-15-2025). For help: >help("RLImatrices") or visit https://centerforassessment.github.io/RLImatrices')
 	}
 }
